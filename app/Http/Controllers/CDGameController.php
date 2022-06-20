7ec1b6b85e475f27155eaaf6426d55689fdf0d66
phamlam2k <mailam1309@gmail.com>
2022-06-20 23:05:00 +0700

    Add validator library

--- app/Http/Controllers/CDGameController.php
+++ app/Http/Controllers/CDGameController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\CDGameService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Validator;
 
 class CDGameController extends Controller
 {
